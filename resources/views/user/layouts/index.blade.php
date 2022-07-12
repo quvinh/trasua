6bdf1d353c4ef912b49a3f61d11f00ad74ec7fc4
quvinh <vinhhp2620@gmail.com>
2022-07-12 23:09:42 +0700

    FE

--- resources/views/user/layouts/index.blade.php
+++ resources/views/user/layouts/index.blade.php
@@ -11,10 +11,10 @@ Trà sữa Tẹt
             <div class="row">
                 <div class="col-md-12">
                     <div id="main-slider" class="owl-carousel owl-theme">
-                        <div class="item"><img src="{{ asset('page/img/main-slider1.jpg') }}" alt="" class="img-fluid"></div>
-                        <div class="item"><img src="{{ asset('page/img/main-slider2.jpg') }}" alt="" class="img-fluid"></div>
-                        <div class="item"><img src="{{ asset('page/img/main-slider3.jpg') }}" alt="" class="img-fluid"></div>
-                        <div class="item"><img src="{{ asset('page/img/main-slider4.jpg') }}" alt="" class="img-fluid"></div>
+                        <div class="item"><img src="{{ asset('page/img/slider1.jpg') }}" alt="" class="img-fluid"></div>
+                        <div class="item"><img src="{{ asset('page/img/slider2.jpg') }}" alt="" class="img-fluid"></div>
+                        <div class="item"><img src="{{ asset('page/img/slider3.jpg') }}" alt="" class="img-fluid"></div>
+                        <div class="item"><img src="{{ asset('page/img/slider4.jpg') }}" alt="" class="img-fluid"></div>
                     </div>
                     <!-- /#main-slider-->
                 </div>
@@ -64,212 +64,53 @@ Trà sữa Tẹt
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
-                            <h2 class="mb-0">Hot this week</h2>
+                            <h2 class="mb-0">Sản phẩm trong tuần</h2>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="container">
+                @php
+                $products = DB::table('products')->where([['visible', '=', 1], ['image', '<>', '']])->take(8)->get();
+                @endphp
                 <div class="product-slider owl-carousel owl-theme">
+                    @foreach($products as $key => $product)
                     <div class="item">
                         <div class="product">
                             <div class="flip-container">
                                 <div class="flipper">
-                                    <div class="front"><a href="detail.html"><img src="img/product1.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="img/product1_2.jpg" alt="" class="img-fluid"></a></div>
+                                    <div class="front"><a href="#"><img src="{{ asset('images/product/'.$product->image) }}" alt="" class="img-fluid"></a></div>
+                                    <div class="back"><a href="#"><img src="{{ asset('images/product/'.$product->image) }}" alt="" class="img-fluid"></a></div>
                                 </div>
-                            </div><a href="detail.html" class="invisible"><img src="img/product1.jpg" alt="" class="img-fluid"></a>
+                            </div><a href="#" class="invisible"><img src="{{ asset('images/product/'.$product->image) }}" alt="" class="img-fluid"></a>
                             <div class="text">
-                                <h3><a href="detail.html">Fur coat with very but very very long name</a></h3>
+                                <h3><a href="#">{{ $product->name }}</a></h3>
                                 <p class="price">
-                                    <del></del>$143.00
+                                    <del></del>{{ number_format($product->price, 0, '', ',').' đ' }}
                                 </p>
                             </div>
                             <!-- /.text-->
-                            <div class="ribbon sale">
+                            <!-- <div class="ribbon sale">
                                 <div class="theribbon">SALE</div>
                                 <div class="ribbon-background"></div>
-                            </div>
+                            </div> -->
                             <!-- /.ribbon-->
+                            @if($key == 0)
                             <div class="ribbon new">
                                 <div class="theribbon">NEW</div>
                                 <div class="ribbon-background"></div>
                             </div>
+                            @endif
                             <!-- /.ribbon-->
-                            <div class="ribbon gift">
+                            <!-- <div class="ribbon gift">
                                 <div class="theribbon">GIFT</div>
                                 <div class="ribbon-background"></div>
-                            </div>
+                            </div> -->
                             <!-- /.ribbon-->
                         </div>
                         <!-- /.product-->
                     </div>
-                    <div class="item">
-                        <div class="product">
-                            <div class="flip-container">
-                                <div class="flipper">
-                                    <div class="front"><a href="detail.html"><img src="img/product2.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="img/product2_2.jpg" alt="" class="img-fluid"></a></div>
-                                </div>
-                            </div><a href="detail.html" class="invisible"><img src="img/product2.jpg" alt="" class="img-fluid"></a>
-                            <div class="text">
-                                <h3><a href="detail.html">White Blouse Armani</a></h3>
-                                <p class="price">
-                                    <del>$280</del>$143.00
-                                </p>
-                            </div>
-                            <!-- /.text-->
-                            <div class="ribbon sale">
-                                <div class="theribbon">SALE</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                            <div class="ribbon new">
-                                <div class="theribbon">NEW</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                            <div class="ribbon gift">
-                                <div class="theribbon">GIFT</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                        </div>
-                        <!-- /.product-->
-                    </div>
-                    <div class="item">
-                        <div class="product">
-                            <div class="flip-container">
-                                <div class="flipper">
-                                    <div class="front"><a href="detail.html"><img src="img/product3.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="img/product3_2.jpg" alt="" class="img-fluid"></a></div>
-                                </div>
-                            </div><a href="detail.html" class="invisible"><img src="img/product3.jpg" alt="" class="img-fluid"></a>
-                            <div class="text">
-                                <h3><a href="detail.html">Black Blouse Versace</a></h3>
-                                <p class="price">
-                                    <del></del>$143.00
-                                </p>
-                            </div>
-                            <!-- /.text-->
-                        </div>
-                        <!-- /.product-->
-                    </div>
-                    <div class="item">
-                        <div class="product">
-                            <div class="flip-container">
-                                <div class="flipper">
-                                    <div class="front"><a href="detail.html"><img src="img/product3.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="img/product3_2.jpg" alt="" class="img-fluid"></a></div>
-                                </div>
-                            </div><a href="detail.html" class="invisible"><img src="img/product3.jpg" alt="" class="img-fluid"></a>
-                            <div class="text">
-                                <h3><a href="detail.html">Black Blouse Versace</a></h3>
-                                <p class="price">
-                                    <del></del>$143.00
-                                </p>
-                            </div>
-                            <!-- /.text-->
-                        </div>
-                        <!-- /.product-->
-                    </div>
-                    <div class="item">
-                        <div class="product">
-                            <div class="flip-container">
-                                <div class="flipper">
-                                    <div class="front"><a href="detail.html"><img src="img/product2.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="img/product2_2.jpg" alt="" class="img-fluid"></a></div>
-                                </div>
-                            </div><a href="detail.html" class="invisible"><img src="img/product2.jpg" alt="" class="img-fluid"></a>
-                            <div class="text">
-                                <h3><a href="detail.html">White Blouse Versace</a></h3>
-                                <p class="price">
-                                    <del></del>$143.00
-                                </p>
-                            </div>
-                            <!-- /.text-->
-                            <div class="ribbon new">
-                                <div class="theribbon">NEW</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                        </div>
-                        <!-- /.product-->
-                    </div>
-                    <div class="item">
-                        <div class="product">
-                            <div class="flip-container">
-                                <div class="flipper">
-                                    <div class="front"><a href="detail.html"><img src="img/product1.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="img/product1_2.jpg" alt="" class="img-fluid"></a></div>
-                                </div>
-                            </div><a href="detail.html" class="invisible"><img src="img/product1.jpg" alt="" class="img-fluid"></a>
-                            <div class="text">
-                                <h3><a href="detail.html">Fur coat</a></h3>
-                                <p class="price">
-                                    <del></del>$143.00
-                                </p>
-                            </div>
-                            <!-- /.text-->
-                            <div class="ribbon gift">
-                                <div class="theribbon">GIFT</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                        </div>
-                        <!-- /.product-->
-                    </div>
-                    <div class="item">
-                        <div class="product">
-                            <div class="flip-container">
-                                <div class="flipper">
-                                    <div class="front"><a href="detail.html"><img src="img/product2.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="img/product2_2.jpg" alt="" class="img-fluid"></a></div>
-                                </div>
-                            </div><a href="detail.html" class="invisible"><img src="img/product2.jpg" alt="" class="img-fluid"></a>
-                            <div class="text">
-                                <h3><a href="detail.html">White Blouse Armani</a></h3>
-                                <p class="price">
-                                    <del>$280</del>$143.00
-                                </p>
-                            </div>
-                            <!-- /.text-->
-                            <div class="ribbon sale">
-                                <div class="theribbon">SALE</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                            <div class="ribbon new">
-                                <div class="theribbon">NEW</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                            <div class="ribbon gift">
-                                <div class="theribbon">GIFT</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                        </div>
-                        <!-- /.product-->
-                    </div>
-                    <div class="item">
-                        <div class="product">
-                            <div class="flip-container">
-                                <div class="flipper">
-                                    <div class="front"><a href="detail.html"><img src="img/product3.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="img/product3_2.jpg" alt="" class="img-fluid"></a></div>
-                                </div>
-                            </div><a href="detail.html" class="invisible"><img src="img/product3.jpg" alt="" class="img-fluid"></a>
-                            <div class="text">
-                                <h3><a href="detail.html">Black Blouse Versace</a></h3>
-                                <p class="price">
-                                    <del></del>$143.00
-                                </p>
-                            </div>
-                            <!-- /.text-->
-                        </div>
-                        <!-- /.product-->
-                    </div>
+                    @endforeach
                     <!-- /.product-slider-->
                 </div>
                 <!-- /.container-->
